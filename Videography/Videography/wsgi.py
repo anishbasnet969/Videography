d7eebb7fdf978f054ce58258a7bd48428921b670
Anish Basnet <anishbasnet969@gmail.com>
2021-06-14 18:11:41 +0545

    Hopefully no more commits

--- Videography/Videography/wsgi.py
+++ Videography/Videography/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nepvine_backend project.
+WSGI config for Videography project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nepvine_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Videography.settings')
 
 application = get_wsgi_application()
